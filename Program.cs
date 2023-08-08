11b89379acb127c86cf2f5095309213ee9c02d6d
TheMith <77514217+TheMithh@users.noreply.github.com>
2023-08-08 14:59:52 -0400

    New_revised versiom

--- Program.cs
+++ Program.cs
@@ -1,8 +1,168 @@
 ﻿using System;
+using System.Linq;
 using static System.Console;
 namespace COMP1202_Ass2_32
 {
+    public class Game
+    {
+        private int itemNumber;
+        private string itemName;
+        private int Price;
+        private int UserRating;
+        private int Quantity;
 
+        public Game(int itemNumber, string itemName, int Price, int UserRating, int Quantity)
+        {
+            this.itemNumber = itemNumber;
+            this.itemName = itemName;
+            this.Price = Price;
+            this.UserRating = UserRating;
+            this.Quantity = Quantity;
+        }
+
+        public int getitemNumber()
+        {
+            return itemNumber;
+        }
+
+        public string getitemName()
+        {
+            return itemName;
+        }
+
+        public int getPrice()
+        {
+            return Price;
+        }
+
+        public int getUserRating()
+        {
+            return UserRating;
+        }
+
+        public int getQuantity()
+        {
+            return Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{itemNumber},{itemName},{Price},{UserRating},{Quantity}";
+        }
+        public static bool CheckItemNumber(string str)
+        {
+            if(str.Length !=4)
+            {
+                return false;
+            }
+            else if(!int.TryParse(str, out _))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static int checkPriceValidation()
+        {
+            Console.WriteLine("Please enter a price for the game.");
+            string userInput = Console.ReadLine();
+            while (!int.TryParse(userInput, out _))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number (double):");
+                userInput = Console.ReadLine();
+            }
+
+            int validDouble = int.Parse(userInput);
+            return validDouble;
+        }
+        static bool IsValidInput(string input)
+        {
+            if (int.TryParse(input, out int number) && number >= 1 && number <= 5)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static int userRatingCheck()
+        {
+            Console.WriteLine("Please enter a number between 1 and 5: ");
+            string userInput = Console.ReadLine();
+
+            while (!IsValidInput(userInput))
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 5:");
+                userInput = Console.ReadLine();
+            }
+            int validNumber = int.Parse(userInput);
+            return validNumber;
+        }
+        public static int GetValidIntegerFromUser()
+        {
+            Console.WriteLine("Please enter an integer: ");
+            string userInput = Console.ReadLine();
+
+            while (!int.TryParse(userInput, out int result))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer:");
+                userInput = Console.ReadLine();
+            }
+
+            return int.Parse(userInput);
+        }
+        public static string ItemNameValidation()
+        {
+            Console.WriteLine("Please enter the name of the item");
+            string userInput = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("The text was empty. Please enter a valid name!!");
+                userInput = Console.ReadLine();
+            }
+            return userInput;
+        }
+    }
+
+    public class Storage
+    {
+
+        public Game[] games;
+        public int maxPrice;
+
+        public Storage(Game[] games)
+        {
+            this.games = games;
+        }
+
+        public void printGame()
+        {
+            foreach (Game game in games)
+            {
+                WriteLine("\nItem Number: ===> " + game.getitemNumber() + "\n");
+                WriteLine("Item Name: ====> " + game.getitemName() + "\n");
+                WriteLine("Price: =====> " + game.getPrice() + "\n");
+                WriteLine("User Rating: =====> " + game.getUserRating() + "\n");
+                WriteLine("Quantity: ========> " + game.getQuantity() + "\n\n");
+            }
+        }
+        public static void addGameToStorage(Game game)
+        {
+
+        }
+
+
+        public void SearchingOnMaxPrice()
+        {
+            Console.WriteLine("Games with price less than or equal to: \n ");
+            maxPrice = Game.GetValidIntegerFromUser(); 
+            foreach (Game game in games)
+            {
+                if (game.getPrice() <= maxPrice) 
+                {
+                    WriteLine(game.ToString());
+                }
+            }
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
@@ -23,28 +183,33 @@ namespace COMP1202_Ass2_32
                 {
                     case 1:
                         Console.WriteLine("We will create a new game in the inventory!");
-                        Console.WriteLine("Do you want to type in the item number, or have it generated? y/n");
+                        Console.WriteLine("Do you know the item number? y/n");
                         string user_choice;
-                        int randomNumber;
+                        int item_number;
+                        string item_name;
+                        int item_price;
+                        int user_rating;
+                        int quantity;
                         while (true)
                         {
                             // We keep promting the user to chose a valid option between yes or no
                             user_choice = Console.ReadLine();
                             if (user_choice == "y")
                             {
-                                Console.WriteLine("Please enter a 4-digit number");
+                                Console.WriteLine("Please enter your 4-digit number");
                                 string input = Console.ReadLine();
                                 while (!Game.CheckItemNumber(input))
                                 {
                                     Console.WriteLine("Please enter a 4 digit number");
                                     input = Console.ReadLine();
                                 }
+                                item_number = Convert.ToInt32(input);
                                 break;
                             }
                             else if (user_choice == "n")
                             {
                                 Random random = new Random();
-                                randomNumber = random.Next(1000, 10000);
+                                item_number = random.Next(1000, 10000);
                                 break;
                             }
                             else
@@ -53,24 +218,19 @@ namespace COMP1202_Ass2_32
                             }
                         }
                         Console.WriteLine("Please type in the item name");
-
-
+                        item_name = Game.ItemNameValidation();
+                        Console.WriteLine("Please enter the price for the item");
+                        item_price = Game.checkPriceValidation();
+                        Console.WriteLine("Please choose a rating for the game, 1-5");
+                        user_rating = Game.userRatingCheck();
+                        quantity = Game.GetValidIntegerFromUser();
+                    // Add the game remaining to the storage...
+                        Game new_game = new Game(item_number, item_name, item_price, user_rating, quantity);
+                        Storage.addGameToStorage(new_game); // to be fixed
+                    case 2:
 
                 }
             }
         }
-
-        static void printGame(Game[] gameList)
-        {
-            foreach (Game g in gameList)
-            {
-                WriteLine("\nItem Number: ===> " + g.getItemNumber() + "\n");
-                WriteLine("Item Name: ====> " + g.getItemName() + "\n");
-                WriteLine("Price: =====> " + g.getPrice() + "\n");
-                WriteLine("User Rating: =====> " + g.getUserRating() + "\n");
-                WriteLine("Quantity: ========> " + g.getQuantity() + "\n\n");
-            }
-        }
-
     }
 }
